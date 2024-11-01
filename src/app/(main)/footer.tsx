4a16f181e27f4b05290530918a19c1028d350693
ryuji-1to <ryuji0222ss@icloud.com>
2024-11-01 17:09:21 +0900

    🔥 remove unused

--- src/app/(main)/footer.tsx
+++ src/app/(main)/footer.tsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
 export function Footer() {
   const start = 2024;
   const currentYear = new Date().getFullYear();
-  const pathname = usePathname();
-  // TODO: レイアウトを見直す
-  if (pathname === "/resume") {
-    return null;
-  }
+
   return (
     <footer className="pt-10 text-center">
       <small>
