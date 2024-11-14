d0725c82f241a41f99e827bbd12b8072687c3a9b
ryuji-1to <ryuji0222ss@icloud.com>
2024-11-14 16:18:02 +0900

    🥅 add error page

--- src/app/(main)/posts/[slug]/page.tsx
+++ src/app/(main)/posts/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { formatMarkdown } from "@/share/lib";
 import matter from "gray-matter";
 import * as v from "valibot";
 import { ArticleLayout } from "../../article-layout";
+import { notFound } from "next/navigation";
 
 const Schema = v.object({
   date: v.date(),
@@ -13,9 +14,16 @@ const Schema = v.object({
 
 async function getPostBySlug(slug: string) {
   const filename = `./public/${slug}/index.md`;
-  const { content, data } = matter(await readFile(filename, "utf8"));
-  const file = await formatMarkdown(content);
-  return v.parse(Schema, { content, ...file, ...data });
+  try {
+    const { content, data } = matter(await readFile(filename, "utf8"));
+    const file = await formatMarkdown(content);
+    return v.parse(Schema, { content, ...file, ...data });
+  } catch (error) {
+    if (error instanceof v.ValiError) {
+      throw new Error("validation failed");
+    }
+    notFound();
+  }
 }
 
 export default async function PostPage(props: {
