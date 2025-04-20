91e6b160b434fcdf14bc3666b24ea2e23b136a91
ryuji-1to <ryuji0222ss@icloud.com>
2025-04-20 14:20:48 +0900

    test

--- src/app/[lang]/resume/page.tsx
+++ src/app/[lang]/resume/page.tsx
@@ -10,5 +10,5 @@ export const metadata: Metadata = {
 export default async function Resume(props: PageLangParam) {
   await initLinguiFromParams(props.params);
   const data = await getFormattedMarkdown("resume.md");
-  return <Markdown>{data.value}</Markdown>;
+  return <Markdown>hoge</Markdown>;
 }
