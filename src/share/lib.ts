822676463d6cabf15c1e21e5fd14575f0eecfc7f
ryuji-1to <ryuji0222ss@icloud.com>
2025-04-20 12:21:35 +0900

    🔬 read file async

--- src/share/lib.ts
+++ src/share/lib.ts
@@ -1,4 +1,4 @@
-import { readFile } from "node:fs/promises";
+import { readdir, readFile } from "node:fs/promises";
 import rehypeShiki from "@shikijs/rehype";
 import { type ClassValue, clsx } from "clsx";
 import matter from "gray-matter";
@@ -8,7 +8,6 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { twMerge } from "tailwind-merge";
 import { unified } from "unified";
-import path from "node:path";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -30,9 +29,9 @@ export async function formatMarkdown(html: string) {
 }
 
 export async function getFormattedMarkdown(filePath: `${string}.md`) {
-  const { content, data } = matter(
-    await readFile(path.join(process.cwd(), `public/${filePath}`), "utf8"),
-  );
+  await readdir("./public/");
+  const f = await readFile(`./public/${filePath}`, "utf8");
+  const { content, data } = matter(f);
   const file = await formatMarkdown(content);
   return { content, ...file, ...data };
 }
