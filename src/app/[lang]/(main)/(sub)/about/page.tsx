51ccc209c28cdd30f6ad57682dec1a356fb2b97a
ryuji-1to <ryuji0222ss@icloud.com>
2025-04-20 13:01:31 +0900

    🐛 fix path

--- src/app/[lang]/(main)/(sub)/about/page.tsx
+++ src/app/[lang]/(main)/(sub)/about/page.tsx
@@ -1,14 +1,25 @@
 import { initLinguiFromParams, type PageLangParam } from "@/app/init-lingui";
 import { Markdown } from "@/share/components/markdown";
-import { getFormattedMarkdown } from "@/share/lib";
+import { formatMarkdown } from "@/share/lib";
+import { readFile } from "node:fs/promises";
+import matter from "gray-matter";
 import type { Metadata } from "next";
+import path from "node:path";
 
 export const metadata: Metadata = {
   title: "Ryuji Ito | About",
 };
 
+async function getFormattedMarkdown() {
+  const { content, data } = matter(
+    await readFile(path.join(process.cwd(), "public/about.md"), "utf8"),
+  );
+  const file = await formatMarkdown(content);
+  return { content, ...file, ...data };
+}
+
 export default async function AboutPage(props: PageLangParam) {
   await initLinguiFromParams(props.params);
-  const data = await getFormattedMarkdown("about.md");
+  const data = await getFormattedMarkdown();
   return <Markdown>{data.value}</Markdown>;
 }
