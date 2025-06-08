dddf8a8424dac9e663c9951c2215ae6ce909842f
ryuji-1to <ryuji0222ss@icloud.com>
2025-06-08 10:27:16 +0900

    🐛

--- src/share/components/tag.tsx
+++ src/share/components/tag.tsx
@@ -2,36 +2,39 @@ import { cn } from "../lib";
 
 type TagWithOnRemove = {
   onClick?: never;
-  onRemove: NonNullable<React.ComponentProps<"button">["onClick"]>;
+  onRemove?: NonNullable<React.ComponentProps<"button">["onClick"]>;
 } & Omit<React.ComponentProps<"span">, "onClick">;
 
 type TagWithOnClick = React.ComponentProps<"button"> & {
-  onClick: NonNullable<React.ComponentProps<"button">["onClick"]>;
+  onClick?: NonNullable<React.ComponentProps<"button">["onClick"]>;
   onRemove?: never;
 };
 
 type Props = TagWithOnClick | TagWithOnRemove;
 
-export function Tag(props: Props) {
+export function Tag({ className, ...props }: Props) {
+  const commonClassName = cn(
+    "bg-gray-4 flex items-center w-fit px-8 py-2 rounded-full",
+    className,
+  );
   if (props.onClick) {
-    const { onClick, children, className, ...restProps } = props;
+    const { onClick, children, ...restProps } = props;
     return (
-      <button
-        {...restProps}
-        className={cn("bg-gray-2 rounded-full", className)}
-        onClick={onClick}
-      >
+      <button {...restProps} className={commonClassName} onClick={onClick}>
         {children}
       </button>
     );
   }
-  const { onRemove, children, className, ...restProps } = props;
+
+  const { onRemove, children, ...restProps } = props;
   return (
-    <span {...restProps} className={cn("bg-gray-2 rounded-full", className)}>
+    <span {...restProps} className={commonClassName}>
       {children}
-      <button type="button" onClick={onRemove}>
-        x
-      </button>
+      {onRemove && (
+        <button type="button" className="ml-4" onClick={onRemove}>
+          x
+        </button>
+      )}
     </span>
   );
 }
